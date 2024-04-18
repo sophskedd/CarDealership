b8a0feb1745997de2bda5f240b69176b70a4be6d
Andrew Huang <ahuang@sdccd.edu>
2024-04-18 12:12:07 -0700

    1. Renamed Dealership.arr to parkingSpots 2. Make the Cars class singular 3. Made the parkingSpots array automatically grow by 10 when full. 4. Renamed Dealership.obtainCar to add 5.  Created src/main/resoures/Cars.csv with mock data 6. Added Dealership.loadCarsFromFile method 7. Added DealershipTest to test automatically growing Dealership.parkingSpots

--- src/main/java/org/example/Dealership.java
+++ src/main/java/org/example/Dealership.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -6,43 +7,65 @@ public class Dealership {
     int numOfCars;
 
 //create array
-    Cars []arr = new Cars[100];
+    Car[] parkingSpots;
 //constructor
     Dealership(){
+         parkingSpots = new Car[10];
     }
-    //obtain cars
-    public void obtainCar(Cars car){
-        for(int i = 0; i < arr.length; i++){
-            if(arr[0] == null){
-                arr[0] = car;
-                break;
-            }
-            else if(arr[i] != null && arr[i+1] == null){
-                arr[i+1] = car;
-                break;
-            }
-            else{
-                System.out.print("Dealership is full.");
-            }
+
+    public void loadCarsFromFile() {
+        Scanner input = new Scanner(Dealership.class.getClassLoader().getResourceAsStream("Cars.csv"));
+        input.nextLine(); // skip header row
+        while(input.hasNextLine()){
+            String[] row = input.nextLine().split(",");
+            Car car = new Car(row[0], Integer.parseInt(row[1]), row[2], row[3], Integer.parseInt(row[4]));
+            add(car);
         }
     }
+
+    /**
+     * Adds a new car to the dealership
+     * @param car the new car
+     */
+    public void add(Car car){
+        // find an open parking spot to park in
+        for(int i = 0; i < parkingSpots.length; i++){
+            if(parkingSpots[i] == null){
+                parkingSpots[i] = car;
+                numOfCars++;
+                return;
+            }
+        }
+        // no parking spots available, add some
+        addTenParkingSpots();
+        add(car);
+    }
+
+    private void addTenParkingSpots() {
+        Car[] newParkingSpots = new Car[parkingSpots.length + 10];
+        for(int i = 0; i < parkingSpots.length; i++){
+            newParkingSpots[i] = parkingSpots[i];
+        }
+        parkingSpots = newParkingSpots;
+    }
+
     //buying the car
     public void purchase(int i){
-        arr[i] = null;
+        parkingSpots[i] = null;
         System.out.println("Enjoy your new car!");
     }
     //test drive the car
-    public void testDrive(Cars car){
+    public void testDrive(Car car){
         Scanner scan = new Scanner(System.in);
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == null){
+        for(int i = 0; i < parkingSpots.length; i++){
+            if(parkingSpots[i] == null){
                 continue;
             }
             //checks if car is at dealership
-            if(arr[i].getBrand().equals(car.getBrand()) && arr[i].getMileage()== car.getMileage() && arr[i].getColor().equals(car.getColor()) && arr[i].getType().equals(car.getType())){
+            if(parkingSpots[i].getBrand().equals(car.getBrand()) && parkingSpots[i].getMileage()== car.getMileage() && parkingSpots[i].getColor().equals(car.getColor()) && parkingSpots[i].getType().equals(car.getType())){
                 System.out.println("Have a good test drive!");
-                arr[i].drive(10);
-                System.out.println("Would you like to buy the car? It is $" + arr[i].getPrice());
+                parkingSpots[i].drive(10);
+                System.out.println("Would you like to buy the car? It is $" + parkingSpots[i].getPrice());
                 String val = scan.nextLine();
                 if(val.equals("yes")||val.equals("Yes")){
                     purchase(i);
@@ -56,22 +79,22 @@ public class Dealership {
     }
     public void browse(){
         //prints array to show all cars
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == null){
+        for(int i = 0; i < parkingSpots.length; i++){
+            if(parkingSpots[i] == null){
                 continue;
             }
-            System.out.printf("Car: %s, Price: $%d, Mileage: %d miles per tank, Color: %s, Type of Vehicle: %s \n",arr[i].getBrand(),arr[i].getPrice(),arr[i].getMileage(),arr[i].getColor(),arr[i].getType());
+            System.out.printf("Car: %s, Price: $%d, Mileage: %d miles per tank, Color: %s, Type of Vehicle: %s \n", parkingSpots[i].getBrand(), parkingSpots[i].getPrice(), parkingSpots[i].getMileage(), parkingSpots[i].getColor(), parkingSpots[i].getType());
         }
     }
 
-    public List<Cars> browseByColor(String color){
-        List<Cars> results = new ArrayList<>();
+    public List<Car> browseByColor(String color){
+        List<Car> results = new ArrayList<>();
         //prints array to show all cars
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == null){
+        for(int i = 0; i < parkingSpots.length; i++){
+            if(parkingSpots[i] == null){
                 continue;
-            } else if(arr[i].getColor().equals(color)){
-                results.add(arr[i]);
+            } else if(parkingSpots[i].getColor().equals(color)){
+                results.add(parkingSpots[i]);
             }
         }
         return results;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         Dealership sd = new Dealership();
-        Cars car1 = new Cars("Honda", 400, "White", "Sedan", 30000);
+        Car car1 = new Car("Honda", 400, "White", "Sedan", 30000);
 
-        sd.obtainCar(car1);
+        sd.add(car1);
 
         sd.browse();
         sd.testDrive(car1);
 
         sd.browse();
 
-        for(Cars whiteCars: sd.browseByColor("Black")) {
+        for(Car whiteCars: sd.browseByColor("Black")) {
             System.out.println(whiteCars);
         }
     }
